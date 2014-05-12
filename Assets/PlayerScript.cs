e0766a6157ce1daf1fb5695f508779320e81c3a0
steverichey <stevenpatrickrichey@gmail.com>
2014-05-12 13:51:55 -0500

    add scripts, player, enemy

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    // The speed of the ship
+    public Vector2 speed = new Vector2(50, 50);
+
+    // Store the movement
+    private Vector2 movement;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    // Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // Movement per direction
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+    // Physics updates should be done here
+    void FixedUpdate()
+    {
+        // Move the game object
+        rigidbody2D.velocity = movement;
+    }
+}
